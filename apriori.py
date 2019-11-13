84db1c7a97d7170dba229a62b57b42587fb3692a
ronnie <subodh.khanger@gmail.com>
2019-11-13 10:38:25 +0100

    update

---
+++ apriori.py
@@ -0,0 +1,152 @@
+import itertools
+import csv
+import para
+
+def readCVSfile(f_name, max_attr=100):
+    
+    with open(f_name, 'r') as f:
+        list_val = list(csv.reader(f, delimiter=','))
+    final_output= []
+    for i in xrange(len(list_val)):
+        list_attr = []
+        for val in list_val[i][1:]:
+            val = val.strip()
+            if int(val) <= max_attr:
+                list_attr.append(val)
+        if len(list_attr) > 0:
+            final_output.append(list_attr)
+    return final_output
+
+
+def runApriori(data, support, confidence):
+    transactions = len(data)
+    hashmap = {}
+    for row in data:
+        for word in row:
+            if word not in hashmap.keys():
+                hashmap[word] = 1
+            else:
+                hashmap[word] += 1
+
+    level = []
+    for key in hashmap:
+        if (100 * hashmap[key] / transactions) >= float(support):
+            level.append([key])
+
+    return findMaximal(level, support, confidence)
+
+
+def subsets(S, m):
+    return set(itertools.combinations(S, m))
+
+
+def frequentItemsets(dataset, prev_l, k):
+    list = subsets(dataset, k)
+    for item in list:
+        s = []
+        for l in item:
+            s.append(l)
+        s.sort()
+        if s not in prev_l:
+            return True
+    return False
+
+
+def apply_support(singles, support):
+    k = 2
+    prev_l = []
+    L = []
+    for item in singles:
+        prev_l.append(item)
+    while prev_l:
+        current = []
+        sets = apriori_simple_imp(prev_l, k - 1)
+        for c in sets:
+            cnt = 0
+            trans = len(data)
+            s = set(c)
+            for T in data:
+                t = set(T)
+                if s.issubset(t):
+                    cnt += 1
+            if (100 * cnt / trans) >= float(support):
+                c.sort()
+                current.append(c)
+        prev_l = []
+        for l in current:
+            prev_l.append(l)
+        k += 1
+        if current:
+            L.append(current)
+    # for item in L:
+    #     for x in item:
+    #         print x
+    return L
+
+
+def findMaximal(singles, support, confidence):
+    
+    num = 1
+    L = apply_support(singles, support)
+    result = 0
+    for list in L:
+        for l in list:
+            length = len(l)
+            count = 1
+            while count < length:
+                r = subsets(l, count)
+                count += 1
+                for item in r:
+                    inc1 = 0
+                    inc2 = 0
+                    s = []
+                    m = []
+                    for i in item:
+                        s.append(i)
+                    for T in data:
+                        if set(s).issubset(set(T)):
+                            inc1 += 1
+                        if set(l).issubset(set(T)):
+                            inc2 += 1
+                    if 100 * inc2 / inc1 >= float(confidence):
+                        for index in l:
+                            if index not in s:
+                                m.append(index)
+                        #print(s, " ==> ", set(l) - set(s))
+                        left = ','.join(s)
+                        right = ','.join(set(l) - set(s))
+                        print (' ==> '.join([left, right]))
+                        result += 1
+                        num += 1
+
+    print('Total rules generated:', result)
+    return result
+
+
+def apriori_simple_imp(prev_l, k):
+    list_len = k
+    l_previous=prev_l
+    result = []
+    for data_list_one in l_previous:
+        for data_list_two in l_previous:
+            count_list = 0
+            item_list_a= []
+            if data_list_one != data_list_two:
+                while count_list < list_len - 1:
+                    if data_list_one[count_list] != data_list_two[count_list]:
+                        break
+                    else:
+                        count_list += 1
+                else:
+                    if data_list_one[list_len - 1] < data_list_two[list_len - 1]:
+                        for item in data_list_one:
+                            item_list_a.append(item)
+                        item_list_a.append(data_list_two[list_len - 1])
+                        if not frequentItemsets(item_list_a, prev_l, k):
+                            result.append(item_list_a)
+    return result
+
+
+
+data = readCVSfile('out1.csv')
+runApriori(data, para.SUPPORT, para.CONFIDENCE)
---
+++ aprioriHTree.py
@@ -0,0 +1,219 @@
+import csv, itertools, para
+
+
+def readCVSfile(filename):
+   
+    reader = csv.reader(open(filename, 'r'), delimiter=',')
+    trans = [map(int, row[1:]) for row in reader]
+    return trans
+
+
+def frequentItem(data_set, support):
+    
+    candidate_one = {}
+    total = len(data_set)
+    for row in data_set:
+        for val in row:
+            if val in candidate_one:
+                candidate_one[val] += 1
+            else:
+                candidate_one[val] = 1
+
+    frequent_1 = []
+    for key, cnt in candidate_one.items():
+        # check if given item has sufficient count.
+        if cnt >= (support * total / 100):
+            frequent_1.append(([key], cnt))
+    return frequent_1
+
+
+class HashNode:
+   
+    def __init__(self):
+        self.children = {}
+        self.isLeaf = True
+        self.bucket = {}
+
+
+class HashTree:
+    
+    def __init__(self, max_leaf_cnt, max_child_cnt):
+        self.root = HashNode()
+        self.max_leaf_cnt = max_leaf_cnt
+        self.max_child_cnt = max_child_cnt
+        self.frequent_itemsets = []
+
+    def recur_insert(self, node, itemset, index, cnt):
+        
+        if index == len(itemset):
+            # last bucket so just insert
+            if itemset in node.bucket:
+                node.bucket[itemset] += cnt
+            else:
+                node.bucket[itemset] = cnt
+            return
+
+        if node.isLeaf:
+
+            if itemset in node.bucket:
+                node.bucket[itemset] += cnt
+            else:
+                node.bucket[itemset] = cnt
+            if len(node.bucket) == self.max_leaf_cnt:
+               
+                for old_itemset, old_cnt in node.bucket.iteritems():
+
+                    hash_key = self.hash(old_itemset[index])
+                    if hash_key not in node.children:
+                        node.children[hash_key] = HashNode()
+                    self.recur_insert(node.children[hash_key], old_itemset, index + 1, old_cnt)
+              
+                del node.bucket
+                node.isLeaf = False
+        else:
+            hash_key = self.hash(itemset[index])
+            if hash_key not in node.children:
+                node.children[hash_key] = HashNode()
+            self.recur_insert(node.children[hash_key], itemset, index + 1, cnt)
+
+    def insert(self, itemset):
+        
+        itemset = tuple(itemset)
+        self.recur_insert(self.root, itemset, 0, 0)
+
+    def add_support(self, itemset):
+        runner = self.root
+        itemset = tuple(itemset)
+        index = 0
+        while True:
+            if runner.isLeaf:
+                if itemset in runner.bucket:
+                    runner.bucket[itemset] += 1
+                break
+            hash_key = self.hash(itemset[index])
+            if hash_key in runner.children:
+                runner = runner.children[hash_key]
+            else:
+                break
+            index += 1
+
+    def dfs(self, node, support_cnt):
+        if node.isLeaf:
+            for key, value in node.bucket.iteritems():
+                if value >= support_cnt:
+                    self.frequent_itemsets.append((list(key), value))
+                    # print key, value, support_cnt
+            return
+
+        for child in node.children.values():
+            self.dfs(child, support_cnt)
+
+    def get_frequent_itemsets(self, support_cnt):
+       
+        self.frequent_itemsets = []
+        self.dfs(self.root, support_cnt)
+        return self.frequent_itemsets
+
+    def hash(self, val):
+        return val % self.max_child_cnt
+
+
+def hashTree(candidate_itemsets, length, max_leaf_cnt=4, max_child_cnt=5):
+  
+    htree = HashTree(max_child_cnt, max_leaf_cnt)
+    for itemset in candidate_itemsets:
+       
+        htree.insert(itemset)
+    return htree
+
+
+def generateKSubsets(dataset, length):
+    subsets = []
+    for itemset in dataset:
+        subsets.extend(map(list, itertools.combinations(itemset, length)))
+    return subsets
+
+
+def checkPrefix(list_1, list_2):
+    for i in range(len(list_1) - 1):
+        if list_1[i] != list_2[i]:
+            return False
+    return True
+
+
+def aprioriFrequentItemsets(dataset, support):
+    
+    support_cnt = int(support / 100.0 * len(dataset))
+    all_frequent_itemsets = frequentItem(dataset, support)
+    prev_frequent = [x[0] for x in all_frequent_itemsets]
+    length = 2
+    while len(prev_frequent) > 1:
+        new_candidates = []
+        for i in range(len(prev_frequent)):
+            j = i + 1
+            while j < len(prev_frequent) and checkPrefix(prev_frequent[i], prev_frequent[j]):
+               #sorting
+                new_candidates.append(prev_frequent[i][:-1] +
+                                      [prev_frequent[i][-1]] +
+                                      [prev_frequent[j][-1]]
+                                      )
+                j += 1
+
+        #  find frequent itemsets
+        h_tree = hashTree(new_candidates, length)
+       
+        k_subsets = generateKSubsets(dataset, length)
+
+     
+        for subset in k_subsets:
+            h_tree.add_support(subset)
+
+       
+        new_frequent = h_tree.get_frequent_itemsets(support_cnt)
+        all_frequent_itemsets.extend(new_frequent)
+        prev_frequent = [tup[0] for tup in new_frequent]
+        prev_frequent.sort()
+        length += 1
+
+    return all_frequent_itemsets
+
+
+def generateRules(f_itemsets, confidence):
+  
+
+    hash_map = {}
+    for itemset in f_itemsets:
+        hash_map[tuple(itemset[0])] = itemset[1]
+
+    a_rules = []
+    for itemset in f_itemsets:
+        length = len(itemset[0])
+        if length == 1:
+            continue
+
+        union_support = hash_map[tuple(itemset[0])]
+        for i in range(1, length):
+
+            lefts = map(list, itertools.combinations(itemset[0], i))
+            for left in lefts:
+                conf = 100.0 * union_support / hash_map[tuple(left)]
+                if conf >= confidence:
+                    a_rules.append([left,list(set(itemset[0]) - set(left)), conf])
+    return a_rules
+
+
+def print_final_value(rules):
+
+    for item in rules:
+        left = ','.join(map(str, item[0]))
+        right = ','.join(map(str, item[1]))
+        print (' ==> '.join([left, right]))
+    print('Total Rules Generated: ', len(rules))
+
+if __name__ == '__main__':
+    transactions = readCVSfile('out1.csv')
+  
+    frequent = aprioriFrequentItemsets(transactions, para.SUPPORT)
+
+    a_rules = generateRules(frequent, para.CONFIDENCE)
+    print_final_value(a_rules)
